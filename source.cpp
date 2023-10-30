d74f8127017d5dc10335d93b5bc8467eb9106e40
khaixa <n8tennyson@gmail.com>
2023-10-29 20:15:27 -0400

    102923 Final

--- source.cpp
+++ source.cpp
@@ -27,39 +27,70 @@ private:
     
     HanoiRecordLink* _top;
 
-    public:
-        stack() {
-            _top = nullptr;
-        }
+public:
+    stack() {
+        _top = nullptr;
+    }
 
-        void push(HanoiRecord record);
-        HanoiRecord pop();
-        HanoiRecord top() {
-        /* missing code */
-        }
-
-        bool is_empty(){
-            return _top == nullptr;
-
-        }
+    void push(HanoiRecord record);
+    HanoiRecord pop();
+    HanoiRecord top();
+    bool is_empty();
  };
 
  void stack::push(HanoiRecord record) {
- /* missing code */
-    }
+    HanoiRecordLink* newLink = new HanoiRecordLink{record, _top};
+    _top = newLink;
+}
     
  HanoiRecord stack::pop() {
- 
-/* missing code */
+    assert(!is_empty() && "The stack is empty");
+    HanoiRecordLink* poppedLink = _top;
+    _top = poppedLink->next;
+    HanoiRecord poppedRecord = poppedLink->value;
+    delete poppedLink;
+    return poppedRecord;    
 }
- //-----------end of stack class definition------------
+
+HanoiRecord stack::top(){
+    assert(!is_empty() && "The stack is empty");
+    return _top->value;
+}
+
+bool stack::is_empty(){
+            return _top == nullptr;
+}
+
+void moveDisk(int sourcePole, int destinationPole){
+    std::cout << "Move disk from pole " << sourcePole << " to pole " << destinationPole << std::endl;
+}
+
+void hanoiIterative(int n){
+    stack stack;
+    stack.push(HanoiRecord(n, 1, 2, 3));
+
+    while (!stack.is_empty()) {
+        HanoiRecord current = stack.pop();
+
+        if (current._numberDisks == 1) {
+            moveDisk(current._sourcePole, current._destinationPole);
+        } else {
+            stack.push(HanoiRecord(current._numberDisks - 1, current._sparePole, current._sourcePole, current._destinationPole));
+            stack.push(HanoiRecord(1, current._sourcePole, current._sparePole, current._destinationPole));
+            stack.push(HanoiRecord(current._numberDisks - 1, current._sourcePole, current._destinationPole, current._sparePole));
+        }
+    }
+} 
+
+//-----------end of stack class definition------------
 int main() {
-    int numberOfDisks = 0;
+    int numberOfDisks;
     HanoiRecord tRecord;
     stack HanoiStack;
-    std::cout << "How many disks? " << std::flush;
+    std::cout << "How many disks?: " << std::flush;
     std::cin >> numberOfDisks;
     
-    HanoiStack.push(HanoiRecord(numberOfDisks, 1, 2, 3));
- /* missing code */
+    hanoiIterative(numberOfDisks);
+
+    return 0;
  }
